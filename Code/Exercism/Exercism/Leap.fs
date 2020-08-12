92408ebbca831d4afbe5b57b5d0474ca80b835ed
grandangelo <grandangelo@zoho.com>
2020-08-12 19:21:41 +0200

    Updated leap solution. Adding queen attack.

--- Code/Exercism/Exercism/Leap.fs
+++ Code/Exercism/Exercism/Leap.fs
@@ -1,11 +1,6 @@
 ﻿module Leap
 
 let leapYear (year: int): bool = 
-    if year % 4 = 0 then
-        if year % 100 = 0 then
-            if year % 400 = 0 then
-                true
-            else false
-        else true
-    else
-        false
+    if year % 400 = 0 then true
+    elif year % 100 = 0 then false
+    else year % 4 = 0
