2c07b902b8dfcd0f81549e7aa125f605bd52b8e1
grandangelo <grandangelo@zoho.com>
2020-08-17 16:24:15 +0200

    Adapted to map instead of dict. Dictionary seems cleaner solution.

--- Code/Exercism/Exercism/Raindrops.fs
+++ Code/Exercism/Exercism/Raindrops.fs
@@ -1,15 +1,16 @@
 ﻿module Raindrops
 
-let private factorsToDrops = [(3, "Pling");
-                              (5, "Plang");
-                              (7, "Plong")]
+let private factorsToDrops = Map[3, "Pling";
+                                 5, "Plang";
+                                 7, "Plong"]
 
 let private isFactor number target =
     number % target = 0
 
 let convert (number: int): string = 
     let sounds = factorsToDrops
-                |> Seq.filter (fun (k, v) -> number % k = 0) 
+                |> Map.filter (fun key value -> number % key = 0) 
+                |> Map.toSeq
                 |> Seq.map snd
                 |> String.concat ""
     if sounds.Length <> 0 then sounds
