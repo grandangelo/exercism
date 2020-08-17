ce346418a22a155118c744f2f5b1979e34d2f597
grandangelo <grandangelo@zoho.com>
2020-08-17 11:45:31 +0200

    Adding raindrops.

---
+++ Code/Exercism/Exercism/Raindrops.fs
@@ -0,0 +1,13 @@
+﻿module Raindrops
+
+let private isFactor target divider =
+    target % divider = 0
+
+let private getNote factor =
+    match factor with
+    | 3 -> "Pling"
+    | 5 -> "Plang"
+    | 7 -> "Plong"
+    | _ -> failwith "Unsupported factor"
+
+let convert (number: int): string = failwith "You need to implement this function."
