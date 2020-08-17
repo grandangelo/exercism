4de3ac048be9142e09537aeb770d0a88ba5bd2c3
grandangelo <grandangelo@zoho.com>
2020-08-17 15:47:10 +0200

    Bad solution, using mutables.

--- Code/Exercism/Exercism/Raindrops.fs
+++ Code/Exercism/Exercism/Raindrops.fs
@@ -1,13 +1,16 @@
 ﻿module Raindrops
 
-let private isFactor target divider =
-    target % divider = 0
+let private factorsToDrops = Map[3, "Pling";
+                                 5, "Plang";
+                                 7, "Plong"]
 
-let private getNote factor =
-    match factor with
-    | 3 -> "Pling"
-    | 5 -> "Plang"
-    | 7 -> "Plong"
-    | _ -> failwith "Unsupported factor"
+let private isFactor number target =
+    number % target = 0
 
-let convert (number: int): string = failwith "You need to implement this function."
+let convert (number: int): string = 
+    let mutable sounds = ""
+    for KeyValue(factor, sound) in factorsToDrops do
+        if isFactor number factor then sounds <- sounds + sound
+    if sounds = "" then
+        sounds <- number.ToString()
+    sounds
