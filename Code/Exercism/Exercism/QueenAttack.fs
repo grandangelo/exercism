9266009db230b134e6a248593154c3a4332a4599
grandangelo <grandangelo@zoho.com>
2020-08-17 11:33:54 +0200

    Queen attack done.

--- Code/Exercism/Exercism/QueenAttack.fs
+++ Code/Exercism/Exercism/QueenAttack.fs
@@ -6,7 +6,18 @@ let private isValidIndex (index: int) =
 let private isValidPosition (position: int * int) =
     isValidIndex(fst position) && isValidIndex(snd position)
 
+let private isSameLine (queen1: int * int) (queen2: int * int) =
+    fst queen1 = fst queen2 || snd queen1 = snd queen2
+
+let private isSameDiagonal (queen1: int * int) (queen2: int * int) =
+    abs(fst queen1 - fst queen2) = abs(snd queen1 - snd queen2)
+
+let private isSameSquare (queen1: int * int) (queen2: int * int) =
+    queen1 = queen2
+
 let create (position: int * int) = 
     isValidPosition(position)
 
-let canAttack (queen1: int * int) (queen2: int * int) = failwith "You need to implement this function."
+let canAttack (queen1: int * int) (queen2: int * int) = 
+    if isSameSquare queen1 queen2 then failwith "Can not occupy same square"
+    isSameLine queen1 queen2 || isSameDiagonal queen1 queen2
