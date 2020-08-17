4d8dbf3f21745d4216ab4661ecf887863795548a
grandangelo <grandangelo@zoho.com>
2020-08-17 16:25:36 +0200

    Set again dictionary solution.

--- Code/Exercism/Exercism/Raindrops.fs
+++ Code/Exercism/Exercism/Raindrops.fs
@@ -1,16 +1,15 @@
 ﻿module Raindrops
 
-let private factorsToDrops = Map[3, "Pling";
-                                 5, "Plang";
-                                 7, "Plong"]
+let private factorsToDrops = [(3, "Pling");
+                              (5, "Plang");
+                              (7, "Plong")]
 
 let private isFactor number target =
     number % target = 0
 
 let convert (number: int): string = 
     let sounds = factorsToDrops
-                |> Map.filter (fun key value -> number % key = 0) 
-                |> Map.toSeq
+                |> Seq.filter (fun (key, value) -> number % key = 0) 
                 |> Seq.map snd
                 |> String.concat ""
     if sounds.Length <> 0 then sounds
