4ba32284c03e9a176ca5ee868e5f4c195dd1c9ef
grandangelo <grandangelo@zoho.com>
2020-08-17 19:20:10 +0200

    Tests not compiling.

--- Code/Exercism/Exercism/Accumulate.fs
+++ Code/Exercism/Exercism/Accumulate.fs
@@ -1,7 +1,12 @@
 ﻿module Accumulate
 
-
+let rec private applyFunction (func: 'a -> 'b) (input: 'a list) (currentResult: 'b list): 'b list =
+    match input with
+    | [] -> currentResult
+    | _ -> let newResult = Seq.append currentResult (func input.Head) |> Seq.toList 
+           applyFunction func input.Tail newResult
 
 let accumulate (func: 'a -> 'b) (input: 'a list): 'b list = 
-    let result = Seq.map func input
-    Seq.toList result
+    let result = []
+    applyFunction func input result
+
