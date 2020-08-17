59a620675b4a7062e2358db79f73827a266d6d04
grandangelo <grandangelo@zoho.com>
2020-08-17 17:01:59 +0200

    Accumulate solution done with Map.

---
+++ Code/Exercism/Exercism/Accumulate.fs
@@ -0,0 +1,7 @@
+﻿module Accumulate
+
+
+
+let accumulate (func: 'a -> 'b) (input: 'a list): 'b list = 
+    let result = Seq.map func input
+    Seq.toList result
--- Code/Exercism/Exercism/Raindrops.fs
+++ Code/Exercism/Exercism/Raindrops.fs
@@ -4,9 +4,6 @@ let private factorsToDrops = [(3, "Pling");
                               (5, "Plang");
                               (7, "Plong")]
 
-let private isFactor number target =
-    number % target = 0
-
 let convert (number: int): string = 
     let sounds = factorsToDrops
                 |> Seq.filter (fun (key, value) -> number % key = 0) 
