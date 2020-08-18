ed6f2c66e1147b775f5a8c3ae079169a7f05e1a8
grandangelo <grandangelo@zoho.com>
2020-08-18 18:40:24 +0200

    Done without unit of measures.

--- Code/Exercism/Exercism/SpaceAge.fs
+++ Code/Exercism/Exercism/SpaceAge.fs
@@ -1,8 +1,5 @@
 ﻿module SpaceAge
 
-[<Measure>]
-type EarthYear
-
 type Planet =
     | Mercury
     | Venus
@@ -15,13 +12,17 @@ type Planet =
 
 let private GetOrbitalPeriod (planet: Planet) =
     match planet with
-    | Mercury -> 0.2408467<EarthYear>
-    | Venus -> 0.61519726<EarthYear>
-    | Earth -> 1.0<EarthYear>
-    | Mars -> 1.8808158<EarthYear>
-    | Jupiter -> 11.862615<EarthYear>
-    | Saturn -> 29.447498<EarthYear>
-    | Uranus -> 84.016846<EarthYear>
-    | Neptune -> 164.79132<EarthYear>
+    | Mercury -> 0.2408467
+    | Venus -> 0.61519726
+    | Earth -> 1.0
+    | Mars -> 1.8808158
+    | Jupiter -> 11.862615
+    | Saturn -> 29.447498
+    | Uranus -> 84.016846
+    | Neptune -> 164.79132
 
-let age (planet: Planet) (seconds: int64): float = failwith "You need to implement this function."
+let private GetEarthYears (seconds: int64) :float =
+    float(seconds) / float(31557600)
+
+let age (planet: Planet) (seconds: int64): float = 
+    (GetEarthYears seconds) / (GetOrbitalPeriod planet)
