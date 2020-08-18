a6ddcdee52b50f966db91f96803aaf702dd395b1
grandangelo <grandangelo@zoho.com>
2020-08-18 17:07:28 +0200

    Understood.

--- Code/Exercism/Exercism/Accumulate.fs
+++ Code/Exercism/Exercism/Accumulate.fs
@@ -3,7 +3,8 @@
 let rec private applyFunc (accumulator: 'b list) (func: 'a -> 'b) (input: 'a list): 'b list =
     match input with
     | [] -> accumulator
-    | head::tail -> applyFunc ((accumulator::(func head)) (func) (tail)
+    | head::tail -> applyFunc (func(head)::accumulator) (func) (tail)
 
 let accumulate (func: 'a -> 'b) (input: 'a list): 'b list = 
     applyFunc [] func input
+    |> applyFunc [] id
