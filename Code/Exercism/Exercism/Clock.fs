63986059c32fd6df6833381fbd48943a2ab834f1
grandangelo <grandangelo@zoho.com>
2020-08-28 15:18:54 +0200

    Done using DateTime from .NET.

--- Code/Exercism/Exercism/Clock.fs
+++ Code/Exercism/Exercism/Clock.fs
@@ -2,15 +2,18 @@ module Clock
 
 open System
 
-let create hours minutes = 
-    let targetHours = ((minutes / 60) + hours) % 24
-    new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day,
-                 targetHours, minutes % 60, 0)
+let private setDate (dateTime: DateTime) = 
+    new DateTime(1970, 1, 1, dateTime.Hour, dateTime.Minute, 0)
 
+let create (hours: int) (minutes: int) = 
+    let startTime = new DateTime(1970, 1, 1, 0, 0, 0)
+    startTime.AddHours(float(hours)).AddMinutes(float(minutes)) |> setDate
 
-let add minutes clock = failwith "You need to implement this function."
+let add (minutes: int) (clock: DateTime) = 
+    clock.AddMinutes(float(minutes))
 
-let subtract minutes clock = failwith "You need to implement this function."
+let subtract (minutes: int) (clock: DateTime) = 
+    add (minutes * -1) clock
 
 let display (clock: DateTime) = 
     sprintf "%s" (clock.ToString("HH:mm"))
