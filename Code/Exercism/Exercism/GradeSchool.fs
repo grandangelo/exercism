0f6090e4774880243b077d96a6f97be70ea1e335
grandangelo <grandangelo@zoho.com>
2020-08-28 11:57:19 +0200

    Even easier when you know the solution.

--- Code/Exercism/Exercism/GradeSchool.fs
+++ Code/Exercism/Exercism/GradeSchool.fs
@@ -11,12 +11,10 @@ let add (student: string) (grade: int) (school: School): School =
     school.Add(grade, student::currentStudents |> List.sort)
     
 let roster (school: School): string list = 
-    let mutable studentsList = []
-    for KeyValue(classroom, students) in school do
-        for student in students do 
-            studentsList <- student::studentsList
-            
-    studentsList |> List.rev
+    school
+    |> Map.toList
+    |> List.map (fun (_, students) -> students)
+    |> List.fold List.append []
 
 let grade (number: int) (school: School): string list =
     match school.TryFind number with
