b3f89d09372722e4599ffbcabb000a047c6e62f8
grandangelo <grandangelo@zoho.com>
2020-08-28 11:10:19 +0200

    Such a hard time for a simple exercise... How to avoid mutables?

--- Code/Exercism/Exercism/GradeSchool.fs
+++ Code/Exercism/Exercism/GradeSchool.fs
@@ -2,10 +2,20 @@
 
 type School = Map<int, string list>
 
-let empty: School = failwith "You need to implement this function."
+let empty: School = Map.empty
 
-let add (student: string) (grade: int) (school: School): School = failwith "You need to implement this function."
+let add (student: string) (grade: int) (school: School): School = 
+    let currentStudents = match school.TryFind grade with
+                          | None -> []
+                          | Some(students) -> students
+    school.Add(grade, student::currentStudents)
+    
+let roster (school: School): string list = 
+    let mutable studentsList = []
+    for KeyValue(classroom, students) in school do
+        for student in students do 
+            studentsList <- student::studentsList
+            
+    studentsList
 
-let roster (school: School): string list = failwith "You need to implement this function."
-
-let grade (number: int) (school: School): string list = failwith "You need to implement this function."
+let grade (number: int) (school: School): string list = failwith "You need to implement this fosse."
