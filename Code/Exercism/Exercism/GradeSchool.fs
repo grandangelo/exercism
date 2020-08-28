390804637549f0672de412ffb752ea16128da483
grandangelo <grandangelo@zoho.com>
2020-08-28 11:29:31 +0200

    So easy with mutables.

--- Code/Exercism/Exercism/GradeSchool.fs
+++ Code/Exercism/Exercism/GradeSchool.fs
@@ -8,7 +8,7 @@ let add (student: string) (grade: int) (school: School): School =
     let currentStudents = match school.TryFind grade with
                           | None -> []
                           | Some(students) -> students
-    school.Add(grade, student::currentStudents)
+    school.Add(grade, student::currentStudents |> List.sort)
     
 let roster (school: School): string list = 
     let mutable studentsList = []
@@ -16,6 +16,9 @@ let roster (school: School): string list =
         for student in students do 
             studentsList <- student::studentsList
             
-    studentsList
+    studentsList |> List.rev
 
-let grade (number: int) (school: School): string list = failwith "You need to implement this fosse."
+let grade (number: int) (school: School): string list =
+    match school.TryFind number with
+    | None -> []
+    | Some(students) -> students
