3d17637dcf262870e3b1d1b982b5fe63854593b4
grandangelo <grandangelo@zoho.com>
2020-08-31 16:32:31 +0200

    "Bob" solution with Regex.

--- Code/Exercism/Exercism/Bob.fs
+++ Code/Exercism/Exercism/Bob.fs
@@ -1,3 +1,13 @@
 ﻿module Bob
 
-let response (input: string): string = failwith "You need to implement this function."
+open System.Text.RegularExpressions
+
+let response (input: string): string = 
+    let trimmedInput = input.Trim()
+    if Regex.IsMatch(input, "^([ \r\n\t])+$") || input = "" then "Fine. Be that way!"
+    else if Regex.IsMatch(trimmedInput, "^([^A-Z])+\?$") then "Sure."
+    else if Regex.IsMatch(trimmedInput, "^([^a-z])+\?$") then "Calm down, I know what I'm doing!"
+    else if Regex.IsMatch(trimmedInput, "^([A-Z0-9,!%\^\*@#\$\(\s])+$") && Regex.IsMatch(trimmedInput, "([A-Z])+") then "Whoa, chill out!"
+    else if trimmedInput.EndsWith('?') then "Sure."
+    else "Whatever."
+
